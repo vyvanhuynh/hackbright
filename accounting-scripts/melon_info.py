c433f71991076ebe37eae21b2c614150d96fb745
vyvan56@hotmail.com <vyvanhuynh@Vys-MacBook-Pro.local>
2021-10-04 15:38:49 -0500

    finished editing both files

--- accounting-scripts/melon_info.py
+++ accounting-scripts/melon_info.py
@@ -1,18 +1,16 @@
 """Print out all the melons in our inventory."""
 
 
-from melons import melon_names, melon_seedlessness, melon_prices
+from melons import melon_data
 
 
-def print_melon(name, seedless, price):
+def print_melon(melon_report):
     """Print each melon with corresponding attribute information."""
+    for melon_name, attributes in melon_data.items():
+        print(melon_name.upper())
 
-    have_or_have_not = 'have'
-    if seedless:
-        have_or_have_not = 'do not have'
-
-    print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
+        for attribute, value in attributes.items():
+            print(f'  {attribute}: {value}')
 
 
-for i in melon_names:
-    print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
+print_melon("melon_report")
--- accounting-scripts/melons.py
+++ accounting-scripts/melons.py
@@ -1,46 +1,37 @@
-melon_names = {
-    1: 'Honeydew',
-    2: 'Crenshaw',
-    3: 'Crane',
-    4: 'Casaba',
-    5: 'Cantaloupe',
-}
-
-melon_prices = {
-    1: 0.99,
-    2: 2.00,
-    3: 2.50,
-    4: 2.50,
-    5: 0.99,
-}
-
-melon_seedlessness = {
-    1: True,
-    2: False,
-    3: False,
-    4: False,
-    5: False,
-}
-
 melon_data = {
     'Honeydew': { 
         'price': 0.99,
-        'seedlessness': True
+        'seedlessness': True,
+        'flesh_color': None,
+        'weight': None,
+        'rind_color': None
     },
     'Crenshaw': {
         'price': 2.00,
-        'seedlessness': False
+        'seedlessness': False,
+        'flesh_color': None,
+        'weight': None,
+        'rind_color': None
     },
     'Crane': {
         'price': 2.50,
-        'seedlessness': False
+        'seedlessness': False,
+        'flesh_color': None,
+        'weight': None,
+        'rind_color': None
     },
     'Casaba': {
         'price': 2.50,
-        'seedlessness': False
+        'seedlessness': False,
+        'flesh_color': None,
+        'weight': None,
+        'rind_color': None
     },
     'Cantaloupe': {
         'price': 0.99,
-        'seedlessness': False
+        'seedlessness': False,
+        'flesh_color': None,
+        'weight': None,
+        'rind_color': None
     }
 }
