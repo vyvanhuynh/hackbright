7b61337ab4d28e3adda5a76ae528f7bd8709a5e4
vyvan56@hotmail.com <vyvanhuynh@Vys-MacBook-Pro.local>
2021-10-04 15:11:09 -0500

    initial commit

---
+++ accounting-scripts/melons.py
@@ -0,0 +1,46 @@
+melon_names = {
+    1: 'Honeydew',
+    2: 'Crenshaw',
+    3: 'Crane',
+    4: 'Casaba',
+    5: 'Cantaloupe',
+}
+
+melon_prices = {
+    1: 0.99,
+    2: 2.00,
+    3: 2.50,
+    4: 2.50,
+    5: 0.99,
+}
+
+melon_seedlessness = {
+    1: True,
+    2: False,
+    3: False,
+    4: False,
+    5: False,
+}
+
+melon_data = {
+    'Honeydew': { 
+        'price': 0.99,
+        'seedlessness': True
+    },
+    'Crenshaw': {
+        'price': 2.00,
+        'seedlessness': False
+    },
+    'Crane': {
+        'price': 2.50,
+        'seedlessness': False
+    },
+    'Casaba': {
+        'price': 2.50,
+        'seedlessness': False
+    },
+    'Cantaloupe': {
+        'price': 0.99,
+        'seedlessness': False
+    }
+}
